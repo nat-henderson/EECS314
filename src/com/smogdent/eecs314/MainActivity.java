179f65a5a2e143d7c0df9afd40b8015840950a07
Evan Gallo <evang10@gmail.com>
2013-05-02 19:31:28 -0400

    adding functionality to load files

--- src/com/smogdent/eecs314/MainActivity.java
+++ src/com/smogdent/eecs314/MainActivity.java
@@ -1,12 +1,14 @@
 package com.smogdent.eecs314;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,20 @@ public class MainActivity extends Activity {
     public void onClick(View view){
         if (view.getId() == R.id.newButton){
             startActivity(new Intent(this, NewProgramActivity.class));
+        }
+        else if(view.getId() == R.id.loadButton){
+        	//check to see if the user can load and save files
+        	String state = Environment.getExternalStorageState();
+        	
+        	if(Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+        		//either we can read or we can read and write, so we can definitely load
+        		//startActivity(new Intent(this, loadProgramActivity.class));
+        	}
+        	else {
+        		//We are in some other state with regards to file storage, best not to load
+        		DialogFragment dialog = new LoadFailedDialogFragment();
+        		dialog.show(getFragmentManager(), "LoadFailedDialogFragment");
+        	}
         }
     }
 
